0a9a6ee61d2a24b8fdf6e98e64c908d64cf8cd0b
TANG-ERIN <33735060+TANG-ERIN@users.noreply.github.com>
2018-08-21 11:19:25 -0400

    hi

--- check_list.js
+++ check_list.js
@@ -73,10 +73,9 @@ function getDiagnosis(formData) {
   var sex = Object.keys(formData.sex)[0];
   var age = formData.age;
   var evidence = [];
-  console.log(formData);
 
   Object.values(formData.symptoms).forEach(function(sym) {
-    console.log(sym);
+
     var obj = {}
     obj["id"] = sym;
     obj["choice_id"] = "present";
@@ -101,7 +100,8 @@ function getDiagnosis(formData) {
 
   function dxComplete() {
     dx_data = JSON.parse(diagnose_request.responseText);
-    console.log(dx_data);
+    console.log(dx_data["conditions"][0]["common_name"]);
+    console.log(dx_data["conditions"][0]["probability"]);
   }
 
 }
@@ -112,3 +112,17 @@ function toggle_visibility(id) {
        else
           e.style.display = 'none';
     }
+    (function () {
+        var old = console.log;
+        var logger = document.getElementById('log');
+        console.log = function () {
+          for (var i = 0; i < arguments.length; i++) {
+            if (typeof arguments[i] == 'object') {
+                logger.innerHTML += (JSON && JSON.stringify ? JSON.stringify(arguments[i], undefined, 2) : arguments[i]) + '<br />';
+            } else {
+                logger.innerHTML += arguments[i] + '<br />';
+            }
+          }
+        }
+    })();
+//Create Labels
