33f0b0314e67ac7fafcf570a7ad5082baa633c41
TANG-ERIN <33735060+TANG-ERIN@users.noreply.github.com>
2018-08-21 09:37:21 -0400

    working checklist

--- check_list.js
+++ check_list.js
@@ -18,16 +18,14 @@ function transferComplete() {
 function populateConditions(data) {
   var conditions_list = document.getElementById('myInput');
   var y = document.getElementById('myForm');
-
   //create an html element for each condition
   data.forEach(function(symptom) {
-    console.log(symptom);
     var div = document.createElement("DIV");
     div.className = "form-check";
     div.setAttribute("id", symptom.name);
     var input = document.createElement("INPUT");
     input.className = "form-check-input";
-    input.setAttribute("name", symptom.name);
+    input.setAttribute("name", "symptoms[" + symptom.name + "]");
     input.setAttribute("type", "checkbox");
     input.setAttribute("value", symptom.id);
     var label = document.createElement("LABEL");
@@ -67,5 +65,42 @@ function myFunction() {
 document.getElementById("myForm").addEventListener("submit", function(e){
     e.preventDefault();
     var formData = $('form').serializeJSON();
-    console.log(formData);
+    getDiagnosis(formData);
 });
+
+function getDiagnosis(formData) {
+  var sex = Object.keys(formData.sex)[0];
+  var age = formData.age;
+  var evidence = [];
+  console.log(formData);
+
+  Object.values(formData.symptoms).forEach(function(sym) {
+    console.log(sym);
+    var obj = {}
+    obj["id"] = sym;
+    obj["choice_id"] = "present";
+    obj["initial"] = true;
+    evidence.push(obj);
+  });
+
+
+  var dig = {
+      'sex':  sex,
+      'age':  age,
+      'evidence' : evidence,
+    }
+    var jsondig= JSON.stringify(dig)
+  var diagnose_request = new XMLHttpRequest();
+  diagnose_request.open('POST', 'https://api.infermedica.com/v2/diagnosis', true);
+  diagnose_request.setRequestHeader("App-Id","00ed7389");
+  diagnose_request.setRequestHeader("App-Key","7f3e634c66e7f5a734a549f7cc4e4683");
+  diagnose_request.setRequestHeader("Content-Type","application/json");
+  diagnose_request.send(jsondig);
+  diagnose_request.addEventListener("load", dxComplete);
+
+  function dxComplete() {
+    dx_data = JSON.parse(diagnose_request.responseText);
+    console.log(dx_data);
+  }
+
+}
